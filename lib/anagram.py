4c5d29adcad57ca3c044d383d49302e4b12a760f
D142-msooh <kinyajuidama176@gmail.com>
2025-11-18 21:31:54 +0300

    Add anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, list_of_anagrams):
+        sorted_word = sorted(self.word)
+        return [anagram for anagram in list_of_anagrams if sorted(anagram.lower()) == sorted_word and anagram.lower() != self.word]
+    
+
+listen = Anagram("listen")
+listen.match(["enlists", "google", "inlets", "banana"])
+print(listen.match(["enlists", "google", "inlets", "banana"]))  # Output should be ['inlets']
